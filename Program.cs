45c2bacce48e63fe47759e33ef57275359b57297
rappy <rappy@riseup.net>
2023-02-19 11:30:19 +0200

    added measurement systems

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
-namespace MyProject
+namespace mmmmmm
 {
     class Program
     {
@@ -25,15 +25,38 @@ namespace MyProject
                 Console.WriteLine("2. Login");
                 Console.Write("Enter your choice (1 or 2): ");
 
-                string choice = Console.ReadLine();
+                string choice = "";
+                while (choice != "1" && choice != "2")
+                {
+                    
+                    choice = Console.ReadLine();
+
+                    if (choice != "1" && choice != "2")
+                    {
+                        Console.WriteLine("Invalid choice. Please enter 1 or 2.");
+                    }
+                }
+
                 // Register
                 if (choice == "1")
                 {
                     Console.Write("Enter a username: ");
                     string username = Console.ReadLine();
+                    while (string.IsNullOrEmpty(username))
+                    {
+                        Console.WriteLine("Username cannot be empty.");
+                        Console.Write("Enter a username: ");
+                        username = Console.ReadLine();
+                    }
 
                     Console.Write("Enter a password: ");
                     string password = Console.ReadLine();
+                    while (string.IsNullOrEmpty(password))
+                    {
+                        Console.WriteLine("Password cannot be empty.");
+                        Console.Write("Enter a password: ");
+                        password = Console.ReadLine();
+                    }
 
                     // Check if the username already exists in the database
                     string checkUserQuery = "SELECT COUNT(*) FROM users WHERE username = @username";
@@ -61,6 +84,9 @@ namespace MyProject
                     {
                         Console.WriteLine($"Error: {ex.Message}");
                     }
+
+                    // Open the BMI calculator
+                    BMICalculator(username, connection);
                 }
 
                 // Login
@@ -68,9 +94,21 @@ namespace MyProject
                 {
                     Console.Write("Enter your username: ");
                     string username = Console.ReadLine();
+                    while (string.IsNullOrEmpty(username))
+                    {
+                        Console.WriteLine("Username cannot be empty.");
+                        Console.Write("Enter your username: ");
+                        username = Console.ReadLine();
+                    }
 
                     Console.Write("Enter your password: ");
                     string password = Console.ReadLine();
+                    while (string.IsNullOrEmpty(password))
+                    {
+                        Console.WriteLine("Password cannot be empty.");
+                        Console.Write("Enter your password: ");
+                        password = Console.ReadLine();
+                    }
 
                     string selectUserQuery = "SELECT * FROM users WHERE username = @username AND password = @password";
                     MySqlCommand selectUserCommand = new MySqlCommand(selectUserQuery, connection);
@@ -82,6 +120,11 @@ namespace MyProject
                         if (reader.HasRows)
                         {
                             Console.WriteLine("Login successful!");
+
+                            reader.Close();
+
+                            // Open the BMI calculator
+                            BMICalculator(username, connection);
                         }
                         else
                         {
@@ -96,8 +139,175 @@ namespace MyProject
                 }
             }
 
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+
+            static void BMICalculator(string username, MySqlConnection connection)
+            {
+                Console.WriteLine($"Welcome to the BMI Calculator, {username}!");
+
+                Console.Write("Please select your measurement system: ");
+                Console.WriteLine("1. Imperial");
+                Console.WriteLine("2. Metric");
+
+                string choice = "";
+                while (choice != "1" && choice != "2")
+                {
+
+                    choice = Console.ReadLine();
+
+                    if (choice != "1" && choice != "2")
+                    {
+                        Console.WriteLine("Invalid choice. Please enter 1 or 2.");
+                    }
+                }
+                // imperial
+                if (choice == "1")
+                {
+                    Console.WriteLine("Enter your height in feet and inches.");
+                    Console.Write("Feet: ");
+                    int feet = int.Parse(Console.ReadLine());
+                    Console.Write("Inches: ");
+                    int inches = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter your weight in pounds.");
+                    Console.Write("Pounds: ");
+                    int pounds = int.Parse(Console.ReadLine());
+
+                    // Calculate the BMI
+                    double heightInInches = feet * 12 + inches;
+                    double bmi = (pounds * 703) / (heightInInches * heightInInches);
+
+                    // Display the BMI and weight status
+                    Console.WriteLine($"Your BMI is {bmi:F2}.");
+                    if (bmi < 18.5)
+                    {
+                        Console.WriteLine("You are underweight.");
+                    }
+                    else if (bmi >= 18.5 && bmi < 25)
+                    {
+                        Console.WriteLine("You are normal weight.");
+                    }
+                    else if (bmi >= 25 && bmi < 30)
+                    {
+                        Console.WriteLine("You are overweight.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are obese.");
+                    }
+
+                    // Get the user ID based on the username
+                    string getUserIdQuery = "SELECT id FROM users WHERE username = @username";
+                    MySqlCommand getUserIdCommand = new MySqlCommand(getUserIdQuery, connection);
+                    getUserIdCommand.Parameters.AddWithValue("@username", username);
+                    int userId = 0;
+                    try
+                    {
+                        object result = getUserIdCommand.ExecuteScalar();
+                        if (result != null)
+                        {
+                            userId = Convert.ToInt32(result);
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        return;
+                    }
+
+                    // Save the BMI data to the database
+                    Console.WriteLine("Saving BMI data to database...");
+                    string insertBmiQuery = "INSERT INTO imperial (user_id, height_ft, height_in, weight_lbs, bmi) " +
+                                            "VALUES (@user_id, @height_ft, @height_in, @weight_lbs, @bmi)";
+                    MySqlCommand insertBmiCommand = new MySqlCommand(insertBmiQuery, connection);
+                    insertBmiCommand.Parameters.AddWithValue("@user_id", userId);
+                    insertBmiCommand.Parameters.AddWithValue("@height_ft", feet);
+                    insertBmiCommand.Parameters.AddWithValue("@height_in", inches);
+                    insertBmiCommand.Parameters.AddWithValue("@weight_lbs", pounds);
+                    insertBmiCommand.Parameters.AddWithValue("@bmi", bmi);
+                    try
+                    {
+                        insertBmiCommand.ExecuteNonQuery();
+                        Console.WriteLine("BMI data saved successfully!");
+                    }
+                    catch (MySqlException ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+
+                }
+                //login
+                else if (choice == "2")
+                {
+                    Console.WriteLine("Enter your height in centimeters.");
+                    Console.Write("Centimeters: ");
+                    double heightCm = double.Parse(Console.ReadLine()) / 100.0;
+
+                    Console.WriteLine("Enter your weight in kilograms.");
+                    Console.Write("Kilograms: ");
+                    double weightKg = double.Parse(Console.ReadLine());
+
+                    // Calculate the BMI
+                    double bmi = weightKg / (heightCm * heightCm);
+
+                    // Display the BMI and weight status
+                    Console.WriteLine($"Your BMI is {bmi:F2}.");
+                    if (bmi < 18.5)
+                    {
+                        Console.WriteLine("You are underweight.");
+                    }
+                    else if (bmi >= 18.5 && bmi < 25)
+                    {
+                        Console.WriteLine("You are normal weight.");
+                    }
+                    else if (bmi >= 25 && bmi < 30)
+                    {
+                        Console.WriteLine("You are overweight.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are obese.");
+                    }
+
+                    // Get the user ID based on the username
+                    string getUserIdQuery = "SELECT id FROM users WHERE username = @username";
+                    MySqlCommand getUserIdCommand = new MySqlCommand(getUserIdQuery, connection);
+                    getUserIdCommand.Parameters.AddWithValue("@username", username);
+                    int userId = 0;
+                    try
+                    {
+                        object result = getUserIdCommand.ExecuteScalar();
+                        if (result != null)
+                        {
+                            userId = Convert.ToInt32(result);
+                        }
+                    }
+                    catch (MySqlException ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        return;
+                    }
+                    // Save the BMI data to the database
+                    Console.WriteLine("Saving BMI data to database...");
+                    string insertBmiQuery = "INSERT INTO metric (user_id, height_cm, weight_kg, bmi) " +
+                                            "VALUES (@user_id, @height_cm, @weight_kg, @bmi)";
+                    MySqlCommand insertBmiCommand = new MySqlCommand(insertBmiQuery, connection);
+                    insertBmiCommand.Parameters.AddWithValue("@user_id", userId);
+                    insertBmiCommand.Parameters.AddWithValue("@height_cm", heightCm);
+                    insertBmiCommand.Parameters.AddWithValue("@weight_kg", weightKg);
+                    insertBmiCommand.Parameters.AddWithValue("@bmi", bmi);
+                    try
+                    {
+                        insertBmiCommand.ExecuteNonQuery();
+                        Console.WriteLine("BMI data saved successfully!");
+                    }
+                    catch (MySqlException ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+
+                }
+
+            }
         }
     }
 }
